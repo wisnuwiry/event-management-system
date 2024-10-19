04b74267e5a33ab88268517be1c407a3d490d4ad
Wisnu G. Saputra <wisnuwiry@gmail.com>
2024-10-19 16:04:56 +0700

    first commit

---
+++ app/Filament/Resources/DonationResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\DonationResource\Pages;
+use App\Filament\Resources\DonationResource\RelationManagers;
+use App\Models\Donation;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class DonationResource extends Resource
+{
+    protected static ?string $model = Donation::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListDonations::route('/'),
+            'create' => Pages\CreateDonation::route('/create'),
+            'edit' => Pages\EditDonation::route('/{record}/edit'),
+        ];
+    }
+}
